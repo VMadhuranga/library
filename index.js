a92ae780dd1dfe6f68260c9648e6b2e3c4cd227b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-17 18:01:41 +0530

    Create function addBookToLibrary
    This function to create a new book using book constructor and
    store in my library array

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+// create a function to create a new book using book constructor and store in my library array
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
