fcdf8ac157a3d275f9a996cfe7871ed16dacef8b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-18 08:59:43 +0530

    Create function displayBook
    This function loop through myLibrary array and display each book
    details on the page
    
    Additional changes:
    
    Add book container to HTML file

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+// create a function that loops through the my library array and displays each book on the page
+let bookContainer = document.querySelector(".book-container");
+
+function displayBook(library) {
+    for (let book of library) {
+        bookTitle = document.createElement("h3");
+        bookTitle.setAttribute("class", "title");
+        bookTitle.textContent = book.title;
+
+        bookAuthor = document.createElement("h3");
+        bookAuthor.setAttribute("class", "author");
+        bookAuthor.textContent = book.author;
+        
+        bookPages = document.createElement("h3");
+        bookPages.setAttribute("class", "pages");
+        bookPages.textContent = book.pages;
+        
+        bookRead = document.createElement("h3");
+        bookRead.setAttribute("class", "read");
+        bookRead.textContent = book.read; 
+
+        bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "book-card");
+
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+        bookContainer.appendChild(bookCard);
+    }
+}
+
+// this is for testing 
+addBookToLibrary("book1", "author1", 11, true);
+addBookToLibrary("book2", "author2", 12, false);
+
+displayBook(myLibrary);
