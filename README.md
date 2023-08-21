ae9be25d542d15a683ca517a464a2d86caa0c443
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-21 11:56:40 +0530

    Format using prettier

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ object constructors, object prototypes and classes in javascript
 Live preview: https://vmadhuranga.github.io/library/
 
 TODO:
+
 - fix can't change last book status if one of previous ones removed
 - fix add book popup is populated with previous book values
--- index.js
+++ index.js
@@ -1,75 +1,77 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-    }
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 
-    readStatus(value) {
-        if (value) {
-            this.read = "Done";
-        } else {
-            this.read = "In Progress";
-        }
+  readStatus(value) {
+    if (value) {
+      this.read = "Done";
+    } else {
+      this.read = "In Progress";
     }
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    if (title && author && pages) {
-        let newBook = new Book(title, author, pages);
-        newBook.readStatus(read);
-        myLibrary.push(newBook);
-    } 
+  if (title && author && pages) {
+    let newBook = new Book(title, author, pages);
+    newBook.readStatus(read);
+    myLibrary.push(newBook);
+  }
 }
 
 let bookContainer = document.querySelector(".book-container");
 
 function displayBook(library) {
-    let bookTitle = document.createElement("h2");
-    bookTitle.setAttribute("class", "title");
+  let bookTitle = document.createElement("h2");
+  bookTitle.setAttribute("class", "title");
 
-    let bookAuthor = document.createElement("p");
-    bookAuthor.setAttribute("class", "author");
+  let bookAuthor = document.createElement("p");
+  bookAuthor.setAttribute("class", "author");
 
-    let bookPages = document.createElement("p");
-    bookPages.setAttribute("class", "pages");
+  let bookPages = document.createElement("p");
+  bookPages.setAttribute("class", "pages");
 
-    let bookReadStatus = document.createElement("p");
-    bookReadStatus.setAttribute("class", "read");
+  let bookReadStatus = document.createElement("p");
+  bookReadStatus.setAttribute("class", "read");
 
-    let changeReadStatusButton = document.createElement("button");
-    changeReadStatusButton.setAttribute("class", "read-status-button");
+  let changeReadStatusButton = document.createElement("button");
+  changeReadStatusButton.setAttribute("class", "read-status-button");
 
-    let removeBookButton = document.createElement("button");
-    removeBookButton.setAttribute("class", "remove-book-button");
+  let removeBookButton = document.createElement("button");
+  removeBookButton.setAttribute("class", "remove-book-button");
 
-    let bookCard = document.createElement("div");
-    bookCard.setAttribute("class", "book-card");
-    
-    for (let book of library) {
-        bookTitle.textContent = book.title;
-        bookAuthor.textContent = `Author: ${book.author}`;
-        bookPages.textContent = `Pages: ${book.pages}`;
-        bookReadStatus.textContent = `Read Status: ${book.read}`;
-        removeBookButton.textContent = "Remove";
-        changeReadStatusButton.textContent = "Change Read Status";
+  let bookCard = document.createElement("div");
+  bookCard.setAttribute("class", "book-card");
 
-        bookCard.appendChild(bookTitle);
-        bookCard.appendChild(bookAuthor);
-        bookCard.appendChild(bookPages);
-        bookCard.appendChild(bookReadStatus);
-        bookCard.appendChild(changeReadStatusButton)
-        bookCard.appendChild(removeBookButton);
-        bookCard.setAttribute("data-book-index", library.indexOf(book))
-        bookContainer.appendChild(bookCard);
-    }
-    changeReadStatus(changeReadStatusButton);
-    removeBookCard(removeBookButton);
+  for (let book of library) {
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = `Author: ${book.author}`;
+    bookPages.textContent = `Pages: ${book.pages}`;
+    bookReadStatus.textContent = `Read Status: ${book.read}`;
+    removeBookButton.textContent = "Remove";
+    changeReadStatusButton.textContent = "Change Read Status";
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookReadStatus);
+    bookCard.appendChild(changeReadStatusButton);
+    bookCard.appendChild(removeBookButton);
+    bookCard.setAttribute("data-book-index", library.indexOf(book));
+    bookContainer.appendChild(bookCard);
+  }
+  changeReadStatus(changeReadStatusButton);
+  removeBookCard(removeBookButton);
 }
 
-let newBookButton = document.querySelector(".header-container .button-new-book");
+let newBookButton = document.querySelector(
+  ".header-container .button-new-book"
+);
 let cancelBookButton = document.querySelector(".form-add-book #cancel-book");
 let formModal = document.querySelector(".form-modal");
 
@@ -77,11 +79,11 @@ newBookButton.addEventListener("click", showFormModal);
 cancelBookButton.addEventListener("click", closeFormModal);
 
 function showFormModal() {
-    formModal.showModal();
+  formModal.showModal();
 }
 
 function closeFormModal() {
-    formModal.close();
+  formModal.close();
 }
 
 let getBookTitle = document.querySelector(".form-add-book #title");
@@ -93,33 +95,33 @@ let addBookButton = document.querySelector(".form-add-book #add-book");
 addBookButton.addEventListener("click", getBookData);
 
 function getBookData() {
-    let setTitle = getBookTitle.value;
-    let setAuthor = getBookAuthor.value;
-    let setPages = Number(getBookPages.value);
-    let setRead = getBookRead.checked;
+  let setTitle = getBookTitle.value;
+  let setAuthor = getBookAuthor.value;
+  let setPages = Number(getBookPages.value);
+  let setRead = getBookRead.checked;
 
-    addBookToLibrary(setTitle, setAuthor, setPages, setRead);
-    displayBook(myLibrary);
-    closeFormModal();
+  addBookToLibrary(setTitle, setAuthor, setPages, setRead);
+  displayBook(myLibrary);
+  closeFormModal();
 }
 
 function removeBookCard(value) {
-    value.addEventListener("click", function() {
-        let index = this.parentElement.dataset.bookIndex;
-        myLibrary.splice(index, 1);
-        this.parentElement.remove();
-    });
+  value.addEventListener("click", function () {
+    let index = this.parentElement.dataset.bookIndex;
+    myLibrary.splice(index, 1);
+    this.parentElement.remove();
+  });
 }
 
 function changeReadStatus(value) {
-    value.addEventListener("click", function() {
-        let index = this.parentElement.dataset.bookIndex;
-        if (myLibrary[index].read === "In Progress") {
-            myLibrary[index].readStatus(myLibrary[index].read === "In Progress");
-            this.previousElementSibling.textContent = `Read Status: ${myLibrary[index].read}`;
-        } else {
-            myLibrary[index].readStatus(myLibrary[index].read === "In Progress");
-            this.previousElementSibling.textContent = `Read Status: ${myLibrary[index].read}`;
-        }
-    });
+  value.addEventListener("click", function () {
+    let index = this.parentElement.dataset.bookIndex;
+    if (myLibrary[index].read === "In Progress") {
+      myLibrary[index].readStatus(myLibrary[index].read === "In Progress");
+      this.previousElementSibling.textContent = `Read Status: ${myLibrary[index].read}`;
+    } else {
+      myLibrary[index].readStatus(myLibrary[index].read === "In Progress");
+      this.previousElementSibling.textContent = `Read Status: ${myLibrary[index].read}`;
+    }
+  });
 }
